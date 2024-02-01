66cfedb58f9d03113233127b2c08269f40e75b47
BhavaniNBL <nblbhavani1995@gmail.com>
2024-02-01 04:46:08 -0600

    Pushing repo to github with configuration of springboot

---
+++ src/main/java/com/dstore/dstorebackend/DstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dstore.dstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dstore/dstorebackend/DstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dstore.dstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
